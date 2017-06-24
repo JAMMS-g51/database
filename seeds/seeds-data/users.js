8862b1ed394c6262f8cb1e3a70a4b8b2e9e9ed08
Matt Pitts <mattpittsmusic@gmail.com>
2017-06-24 11:23:27 -0600

    Made migrations table for users. Made seeds for user table.

---
+++ seeds/seeds-data/users.js
@@ -0,0 +1,12 @@
+module.exports = [
+	{
+		name: 'Billy',
+		email: 'BillyJean@gmail.com',
+		password: 'isnotmygirl'
+	},
+	{
+		name: 'joe',
+		email: 'JoeBob@gmail.com',
+		password: 'isnotmyboy'
+	}
+];
