74157359156fa1d581c67947cc88def1c5eee114
Stephen Warton <stephen.warton@gmail.com>
2017-06-27 09:57:01 -0600

    added production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgresql://localhost/jellodb'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
