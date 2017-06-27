53e4b264a439a3847fa8a485de6c5b82d7c48dc7
Jason Clifton <j.clifton1473@gmail.com>
2017-06-26 18:48:53 -0600

    Seeded extra grouping and story data.

--- seeds/seeds-data/grouping.js
+++ seeds/seeds-data/grouping.js
@@ -2,5 +2,21 @@ module.exports = [
   {
     project_id: 1,
     name: 'column1'
+  },
+  {
+    project_id: 1,
+    name: 'column2'
+  },
+  {
+    project_id: 1,
+    name: 'column3'
+  },
+  {
+    project_id: 1,
+    name: 'column4'
+  },
+  {
+    project_id: 1,
+    name: 'column5'
   }
 ];
--- seeds/seeds-data/story.js
+++ seeds/seeds-data/story.js
@@ -5,5 +5,47 @@ module.exports = [
     difficulty: 5,
     order: 1,
     deadline: new Date()
+  },
+  {
+    grouping_id: 1,
+    name: 'asdjk',
+    difficulty: 5,
+    order: 2,
+    deadline: new Date()
+  },
+  {
+    grouping_id: 1,
+    name: '1',
+    difficulty: 5,
+    order: 3,
+    deadline: new Date()
+  },
+  {
+    grouping_id: 1,
+    name: '3',
+    difficulty: 5,
+    order: 4,
+    deadline: new Date()
+  },
+  {
+    grouping_id: 1,
+    name: 'four',
+    difficulty: 5,
+    order: 5,
+    deadline: new Date()
+  },
+  {
+    grouping_id: 1,
+    name: 'that',
+    difficulty: 5,
+    order: 6,
+    deadline: new Date()
+  },
+  {
+    grouping_id: 1,
+    name: 'story',
+    difficulty: 5,
+    order: 7,
+    deadline: new Date()
   }
 ];
