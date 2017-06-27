9240117d643a6a1b910bc2b9be277305db0f126b
Matthew Victor <vicmcnabby@gmail.com>
2017-06-27 09:52:16 -0600

    updated seeds

--- seeds/seeds-data/link.js
+++ seeds/seeds-data/link.js
@@ -1,7 +1,61 @@
-module.exports = [
-  {
+module.exports = [{
     story_id: 1,
     href: 'google.com',
     title: 'hello'
+  },
+  {
+    story_id: 1,
+    href: 'bing.com',
+    title: 'my link'
+  },
+  {
+    story_id: 2,
+    href: 'trello.com',
+    title: 'competitor'
+  },
+  {
+    story_id: 2,
+    href: 'compuserve.com',
+    title: 'yo'
+  },
+  {
+    story_id: 3,
+    href: 'learn.com',
+    title: 'school'
+  },
+  {
+    story_id: 3,
+    href: 'galvanize.com',
+    title: 'research'
+  },
+  {
+    story_id: 1,
+    href: 'computer.com',
+    title: 'yo dude'
+  },
+  {
+    story_id: 4,
+    href: 'learn2learn.com',
+    title: 'school again'
+  },
+  {
+    story_id: 4,
+    href: 'colorado.com',
+    title: 'where to live'
+  },
+  {
+    story_id: 1,
+    href: 'compuserve.com',
+    title: 'yo'
+  },
+  {
+    story_id: 2,
+    href: 'learn2learn.com',
+    title: 'school stuff'
+  },
+  {
+    story_id: 3,
+    href: 'colorado.com',
+    title: 'where to live'
   }
 ];
--- seeds/seeds-data/list_item.js
+++ seeds/seeds-data/list_item.js
@@ -1,6 +1,61 @@
-module.exports = [
-  {
+module.exports = [{
     list_id: 1,
     content: 'content 1'
+  },
+  {
+    list_id: 1,
+    content: 'content 2'
+  },
+  {
+    list_id: 2,
+    content: 'content 1'
+  },
+  {
+    list_id: 2,
+    content: 'content 2'
+  },
+  {
+    list_id: 3,
+    content: 'content 1'
+  },
+  {
+    list_id: 3,
+    content: 'content 2'
+  },
+  {
+    list_id: 4,
+    content: 'content 1'
+  },
+  {
+    list_id: 4,
+    content: 'content 2'
+  },
+  {
+    list_id: 5,
+    content: 'content 1'
+  },
+  {
+    list_id: 5,
+    content: 'content 2'
+  },
+  {
+    list_id: 1,
+    content: 'content 3'
+  },
+  {
+    list_id: 2,
+    content: 'content 3'
+  },
+  {
+    list_id: 3,
+    content: 'content 3'
+  },
+  {
+    list_id: 4,
+    content: 'content 3'
+  },
+  {
+    list_id: 5,
+    content: 'content 3'
   }
 ];
